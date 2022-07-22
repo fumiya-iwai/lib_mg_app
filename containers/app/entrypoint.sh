a5b31131d271eaffef95fe9054283bdc6a36f166
Harada <g.tales.0817@gmail.com>
2022-07-22 16:06:21 +0900

    環境構築の不備、`InterviewSkillCheck`の置換漏れ

--- containers/app/entrypoint.sh
+++ containers/app/entrypoint.sh
@@ -2,9 +2,9 @@
 set -e
 
 # 初期に作成されるPIDを削除する
-rm -f /interview-skill-check/tmp/pids/server.pid
+rm -f /intern-2022/tmp/pids/server.pid
 
 # DockerfileのCMDにセットしたすべての引数を実行する
 exec "$@"
 
-/interview-skill-check/bin/webpack-dev-server
+/intern-2022/bin/webpack-dev-server
