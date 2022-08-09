4e4c9760eb6cedf6bc5bef2cb7bb6c33e987e808
Ryohei Makabe <makabe@ingage.jp>
2022-08-09 09:58:52 +0900

    図書一覧用のルーティング作成

--- app/assets/javascript/config/router.js
+++ app/assets/javascript/config/router.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from '../components/HomePage.vue'
 import AuthorRegistrationPage from '../components/AuthorRegistrationPage.vue'
 import BookRegistrationPage from '../components/BookRegistrationPage.vue'
+import BookListPage from '../components/BookListPage.vue'
 
 const routes = [
   { path: '',           component: HomePage },
   { path: '/authors/new',   component: AuthorRegistrationPage },
-  { path: '/books/new',     component: BookRegistrationPage }
+  { path: '/books/new',     component: BookRegistrationPage },
+  { path: '/books',    component: BookListPage },
 ];
 
 const router = createRouter({
