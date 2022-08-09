6ed960d6bb6150195c20978d49f3eb242b63d5ca
Harada <g.tales.0817@gmail.com>
2022-08-09 12:45:29 +0900

    著者登録：登録ボタンのdisable制御

--- app/assets/javascript/components/AuthorRegistrationPage.vue
+++ app/assets/javascript/components/AuthorRegistrationPage.vue
@@ -5,10 +5,10 @@
     <form @submit.prevent="onSubmit">
       <div class="field">
         <label>著者名</label>
-        <input v-model="state.name" type="text">
-        <p v-if="!!state.errors['name']" class="error" style="color: red;">{{ state.errors['name'][0]}}</p>
+        <input v-model="state.name" @input="validateAuthorName" type="text">
+        <p v-if="!!state.errors['name']" class="error" style="color: red;">{{ state.errors['name'].join("\n") }}</p>
       </div>
-      <button type="submit">登録する</button>
+      <button type="submit" :disabled="state.validate === false">登録する</button>
     </form>
   </div>
 </template>
@@ -20,7 +20,7 @@ import axios from 'axios';
 export default defineComponent({
   name: "register author",
   setup(_props) {
-    const state = reactive({ name: '', errors: '' });
+    const state = reactive({ name: '', validate: false, errors: '' });
 
     const onSubmit = () => {
       console.log('author name:', state.name);
@@ -38,9 +38,14 @@ export default defineComponent({
         });
     }
 
+    const validateAuthorName = () => {
+      state.validate = state.name.length > 0;
+    }
+
     return {
       state,
-      onSubmit
+      onSubmit,
+      validateAuthorName
     }
   }
 })
