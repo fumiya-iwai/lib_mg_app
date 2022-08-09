ba5cea36873fbc8e8dab4d923f75d4a8f3b4e455
Ryohei Makabe <makabe@ingage.jp>
2022-08-09 09:59:19 +0900

    貸出一覧用のルーティング作成

--- app/assets/javascript/config/router.js
+++ app/assets/javascript/config/router.js
@@ -3,12 +3,14 @@ import HomePage from '../components/HomePage.vue'
 import AuthorRegistrationPage from '../components/AuthorRegistrationPage.vue'
 import BookRegistrationPage from '../components/BookRegistrationPage.vue'
 import BookListPage from '../components/BookListPage.vue'
+import RentalListPage from '../components/RentalListPage.vue'
 
 const routes = [
   { path: '',           component: HomePage },
   { path: '/authors/new',   component: AuthorRegistrationPage },
   { path: '/books/new',     component: BookRegistrationPage },
   { path: '/books',    component: BookListPage },
+  { path: '/rentals',    component: RentalListPage },
 ];
 
 const router = createRouter({
