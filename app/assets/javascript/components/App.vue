b382142d3131e138f4edfe4f9c94d34739a95e8b
shutooike <shutofootball@icloud.com>
2022-08-18 22:09:23 +0900

    App.vue のデザインを整えた

--- app/assets/javascript/components/App.vue
+++ app/assets/javascript/components/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div>
-    <header>
-      <div id="nav">
-        <router-link to="/books">図書一覧</router-link> |
-        <router-link to="/rentals">貸出</router-link> |
-        <router-link to="/books/new">図書登録</router-link> |
-        <router-link to="/authors/new">著者登録</router-link>
-      </div>
-    </header>
+  <a-layout id="app-layout" style="height: 100%; background: #fff;">
+    <a-layout-header style="background: #fff">
+      <a-row type="flex">
+        <a-col >
+          <!-- TODO: 画像に変更 -->
+          <div class="logo" style="  width: 120px; height: 31px; margin: 16px; background: orange;" />
+        </a-col>
+        <a-col flex="auto" justify="end">
+          <a-row type="flex" justify="end">
+            <a-col>
+              <a-menu v-model:selectedKeys="current"
+                      mode="horizontal" style="border: 0">
+                <a-menu-item v-for="item in itemList" :key="item.name">
+                  <router-link :to="{ name: item.name }">{{ item.content }}</router-link>
+                </a-menu-item>
+              </a-menu>
+            </a-col>
+          </a-row>
+        </a-col>
+      </a-row>
+    </a-layout-header>
+    <a-divider style="height: 1px; background-color: #F0F0F0; margin: 0"/>
 
-    <router-view/>
-  </div>
+    <a-layout-content style="padding: 50px;">
+      <router-view/>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   setup () {
+    const itemList = [
+      { name: 'books', content: '図書一覧' },
+      { name: 'rentals', content: '貸出' },
+      { name: 'book', content: '図書登録' },
+      { name: 'author', content: '著者登録' },
+    ]
+    const current = ref([])
+    const router = useRouter();
+    router.afterEach((to) => {
+      current.value[0] = to.name;
+    });
+
     return {
-    }
-  }
+      current,
+      itemList,
+    };
+  },
 })
 </script>
-
-<style scoped>
-</style>
