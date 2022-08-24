20ef3588b2bdaf10f20bf89e345ba2c63690304d
shutooike <shutofootball@icloud.com>
2022-08-24 13:55:11 +0900

    ログイン画面のデザインを整えた

--- app/assets/javascript/packs/application.js
+++ app/assets/javascript/packs/application.js
@@ -3,5 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "../../stylesheets/application.scss";
+import 'logo.svg';
+import 'application.scss';
 
